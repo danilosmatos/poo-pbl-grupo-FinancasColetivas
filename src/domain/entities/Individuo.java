96d697bbc02cb2ef69b6ab68c1b33a2fd6816343
Anton-Gabriel-code <antoniothecode@gmail.com>
2026-06-04 15:30:28 -0300

    entities criada  junto com a classe individuos

---
+++ src/domain/entities/Individuo.java
@@ -0,0 +1,33 @@
+package domain.entities;
+
+import domain.valueobjects.Dinheiro;
+
+public class Individuo {
+    private final int id;
+    private final String nome;
+    private Dinheiro saldo;
+
+    public Individuo(int id, String nome, Dinheiro saldo){
+        this.id = id;
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Dinheiro getSaldo() {
+        return saldo;
+    }
+
+    public void ExibirInfo() {
+
+        double valorEmReais = saldo.getCentavos() / 100.0;
+        System.out.println("Nome:" + nome + " | ID:" + id + " | Saldo: R$" + valorEmReais);
+    }
+}
